82b47437fd8b96e972e394517f1bff4b4e03fa4d
joan <joanflores905@gmail.com>
2022-09-19 17:09:33 +0000

    generador por consola

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".org"];
+let generador = [];
+
+for (let p = 0; p < pronoun.length; p++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let d = 0; d < domain.length; d++) {
+        generador.push(" " + pronoun[p] + adj[a] + noun[d] + domain[d]);
+      }
+    }
+  }
+}
+console.log(generador);
